f6ae24b64ad77cc9de23979d2e58e65ccb761b84
Den Patin <denpatin@gmail.com>
2017-02-24 22:33:48 +0300

    Add seed data (countries + cities)

--- init.bat
+++ init.bat
@@ -8,5 +8,6 @@ CREATE DATABASE IF NOT EXISTS `%3` ^
 CHARACTER SET utf8 ^
 COLLATE utf8_general_ci;
   mysql -u%1 -p%2 -e "!sql!"
-  mysql -u%1 -p%2 %3 < schema.sql
+  set sqls=schema.sql seed.sql
+  for %%a in (%sqls%) do mysql -u%1 -p%2 %3 < %%a
 )
