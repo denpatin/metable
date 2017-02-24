7e08ccf4533770757a7f220131d37e68a74149a3
Den Patin <denpatin@gmail.com>
2017-02-24 22:20:36 +0300

    Introduce the check for argument presence

--- init.bat
+++ init.bat
@@ -1,7 +1,11 @@
-SetLocal enableDelayedExpansion
-set sql=DROP DATABASE IF EXISTS `%3`; ^
+if %3.==. (
+  echo Three parameters are needed: DB user, DB password, and DB name.
+) else (
+  SetLocal enableDelayedExpansion
+  set sql=DROP DATABASE IF EXISTS `%3`; ^
 CREATE DATABASE IF NOT EXISTS `%3` ^
 CHARACTER SET utf8 ^
 COLLATE utf8_general_ci;
-mysql -u%1 -p%2 -e "!sql!"
-mysql -u%1 -p%2 %3 < schema.sql
+  mysql -u%1 -p%2 -e "!sql!"
+  mysql -u%1 -p%2 %3 < schema.sql
+)
