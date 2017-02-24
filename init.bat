f5f9a70eaa64abd3f14a776546bf87a48fd75ba7
Den Patin <denpatin@gmail.com>
2017-02-24 22:13:24 +0300

    Improve style of batch file

--- init.bat
+++ init.bat
@@ -1,3 +1,7 @@
-mysql -u%1 -p%2 -e "DROP DATABASE IF EXISTS `%3`;"
-mysql -u%1 -p%2 -e "CREATE DATABASE IF NOT EXISTS `%3` CHARACTER SET utf8 COLLATE utf8_general_ci;"
+SetLocal enableDelayedExpansion
+set sql=DROP DATABASE IF EXISTS `%3`; ^
+CREATE DATABASE IF NOT EXISTS `%3` ^
+CHARACTER SET utf8 ^
+COLLATE utf8_general_ci;
+mysql -u%1 -p%2 -e "!sql!"
 mysql -u%1 -p%2 %3 < schema.sql
